42da12c459211d7ef034afd246f7e85214a53a21
Richard To <gohn76@gmail.com>
2012-07-27 19:56:54 -0800

    Made large changes to import interface and flow. Added integration with imports api in main repo

--- dashgourd/importer/helper.py
+++ dashgourd/importer/helper.py
@@ -1,10 +1,58 @@
-import os
 import os.path
+import os
 import subprocess
+from dashgourd.importer.mysql import MysqlImporter
 
+def import_users(query_name, query):
+    """Boilerplate for loading user import scripts
+    
+    This uses os environment variables for the mysql and mongodb 
+    uris.
+    
+    Main reason for this is to avoid copying and pasting this boilerplate 
+    code in dozens of query files. Granted you could just have them all 
+    in a single file. That may actually be simpler.
+    
+    Args:
+        query_name: Name of query. Needed for import api to track last_update
+        query: Query to execute with format tokens {start} and {end}
+    """
+    
+    importer = MysqlImporter(os.environ.get('MYSQL_URI'), 
+        os.environ.get('MONGO_URI'), os.environ.get('MONGO_DB'))
+    importer.import_users(query_name, query)
+    importer.close()
+    
+def import_actions(action_name, query, query_name=None):
+    """Boilerplate for loading actions from import scripts
+    
+    See import_users method for more info.
+    """
+    
+    if query_name is None:
+        query_name = "{}_{}".format('action', action_name)
+        
+    importer = MysqlImporter(os.environ.get('MYSQL_URI'), 
+        os.environ.get('MONGO_URI'), os.environ.get('MONGO_DB'))
+    importer.import_actions(action_name, query_name, query)
+    importer.close()
+    
+def import_abtests(abtest, query, query_name=None):
+    """Boilerplate for loading abtests from import scripts
+    
+    See import_users method for more info. 
+    """
 
+    if query_name is None:
+        query_name = "{}_{}".format('ab', abtest)
+            
+    importer = MysqlImporter(os.environ.get('MYSQL_URI'), 
+        os.environ.get('MONGO_URI'), os.environ.get('MONGO_DB'))
+    importer.import_abtests(abtest, query_name, query)
+    importer.close()
+    
 def run_import_scripts(location, args=[], whitelist=None):
-    """Runs import scripts at specified folder location
+    """Runs import scripts at specified folder location.
     
     This is a helper classs in the case that you split import 
     scripts into multiple files. And don't want to manually run 
@@ -17,14 +65,14 @@ def run_import_scripts(location, args=[], whitelist=None):
     instead of sending events as they come.
     
     This assumes the scripts are self contained and will send the 
-    appropriate data to dashgourd.
+    appropriate data to DashGourd.
     
     Args:
         location: Can be a single file or a directory. Not recursive.
         args: An optional list of arguments to pass into the script
         whitelist: A list of files to run in the directory.
     """
-    
+
     cmd_args = [0]
     cmd_args.extend(args)
 
--- dashgourd/importer/mysql.py
+++ dashgourd/importer/mysql.py
@@ -1,8 +1,10 @@
 import os
+from datetime import datetime, timedelta
+from urlparse import urlparse
 import MySQLdb
 from dashgourd.api.actions import ActionsApi
-from dashgourd.api.helper import get_mongodb_db
-from pymongo import Connection
+from dashgourd.api.imports import ImportApi
+from dashgourd.api.helper import init_mongodb
 
 class MysqlImporter(object):
     """Imports data from MySQL to DashGourd.
@@ -11,19 +13,62 @@ class MysqlImporter(object):
     Users cannot be imported with actions yet.
     Actions can be imported if the user exists.
     Also make sure field names are labeled correctly.
-    Users and Actions require an _id attribute.
+    Users and Actions require a user_id attribute.
     
+    MysqlImporter sends data directly to MongoDb using the 
+    actions api. It does not use go through a web service api 
+    endpoint. Seems inefficient for large amounts of data.
+        
     Attributes:
-        mysql_conn: MySQLdb connection
-        api: DashGourd api object  
+        mysql_uri: MySQLdb connection string
+        mongo_uri: MongoDb Connection string
+        mongo_dbname: Name of dashgourd db
+        date_format: MySQL timestamp string format
     """
      
-    def __init__(self, mysql_conn, api):
-        self.mysql_conn = mysql_conn
-        self.api = api
-             
-    def import_users(self, query):
+    def __init__(self, mysql_uri, mongo_uri, mongo_dbname):
+        
+        result = urlparse(mysql_uri)
+        
+        self.mysql_conn = MySQLdb.connect(
+            user=result.username,
+            passwd=result.password,
+            db=result.path[1:],
+            host= result.hostname,
+            port= result.port)
+        
+        mongodb = init_mongodb(mongo_uri, mongo_dbname)
+        self.actions_api = ActionsApi(mongodb)
+        self.import_api = ImportApi(mongodb)
+        
+        self.date_format = '%Y-%m-%d %H:%M:%S'
+
+    def get_daterange(self, query_name):
+        """Gets date range for queries
+        
+        Args:
+            query_name: Name of query to look up in db.
+        
+        Returns:
+            date_range: Dictionary with start and end dates.
+        """
+        
+        start_date = self.import_api.get_last_update(query_name)
+        now = datetime.now()
+        end_date = datetime(
+            now.year, now.month, now.day, now.hour, 0, 0)     
+        
+        return {
+            'start':start_date.strftime(self.date_format), 
+            'end':end_date.strftime(self.date_format)
+        }
+    
+    def import_users(self, query_name, query):
         """Imports users into DashGourd.
+        
+        The query should contain string format tokens for
+        start and end dates. Use {start} and {end}. This is 
+        required even if you plan import all data.
         
         The data will be inserted as is into the user collection.
         This method inserts new users and does not update them.
@@ -33,24 +78,34 @@ class MysqlImporter(object):
         `actions` is reserved for user actions
         
         Note that users are not inserted in batch. 
-        They are inserted one at a time.
+        They are inserted one at a time. This is to avoid 
+        running out of RAM for large imports. But maybe this will 
+        be too slow?
         
         Args:
+            query_name: Name of query for tracking last update
             query: MySQL query to run
         """
             
-        if self.mysql_conn.open:        
+        if self.mysql_conn.open:  
+            date_range = self.get_daterange(query_name)
+            last_update = datetime.strptime(date_range['start'], self.date_format)
+                        
             cursor = self.mysql_conn.cursor(MySQLdb.cursors.DictCursor)
-            cursor.execute(query)
+            cursor.execute(query.format(**date_range))
             numrows = int(cursor.rowcount)
             
             for i in range(numrows):
                 data = cursor.fetchone()
-                self.api.create_user(data)
-                            
+                self.actions_api.create_user(data)
+                
+                if data['created_at'] > last_update:
+                    last_update = data['created_at']
+            
+            self.import_api.set_last_update(query_name, last_update)
             cursor.close()        
         
-    def import_actions(self, name, query):
+    def import_actions(self, action_name, query_name, query):
         """Imports actions into DashGourd
         
         The data will be inserted into the embedded document list named
@@ -61,48 +116,76 @@ class MysqlImporter(object):
         and not insert that row.
         
         Args:
-            name: Action name
+            query_name: Name of query so we can keep track of last update
+            action_name: Action name
             query: MySQL query to run
         """
                 
-        self.api.register_action(name)
-                    
+        self.actions_api.register_action(action_name)
+
         if self.mysql_conn.open:
+            date_range = self.get_daterange(query_name)
+            last_update = datetime.strptime(date_range['start'], self.date_format)
+            
             cursor = self.mysql_conn.cursor(MySQLdb.cursors.DictCursor)
-            cursor.execute(query)
+            cursor.execute(query.format(**date_range))
             numrows = int(cursor.rowcount)
             
             for i in range(numrows):        
                 data = cursor.fetchone()
-                data['name'] = name
-                self.api.insert_action(data)
+                data['name'] = action_name
+                
+                user_id = data['user_id']
+                del data['user_id']
+                
+                self.actions_api.insert_action(user_id, data)
             
+                if data['created_at'] > last_update:
+                    last_update = data['created_at']
+            
+            self.import_api.set_last_update(query_name, last_update)            
             cursor.close() 
 
-    def import_abtests(self, abtest, query):
+    def import_abtests(self, abtest, query_name, query):
         """Imports abtests into DashGourd
         
         The data will be inserted into an object named `ab`
         
-        The data must include the following fields `user_id`, `abtest`, `variation`.
+        The data must include the following fields `user_id`, `abtest`, `variation` 
+        and `created_at`. Created at is only used to keep track of last update time.
         
         If the data does not contain those fields, then the api will fail silently
         and not insert that row.
         
         Args:
+            query_name: Name of query
             abtest: Ab test name
             query: MySQL query to run
         """        
         if self.mysql_conn.open:
+            date_range = self.get_daterange(query_name)
+            last_update = datetime.strptime(date_range['start'], self.date_format)
+            
             cursor = self.mysql_conn.cursor(MySQLdb.cursors.DictCursor)
-            cursor.execute(query)
+            cursor.execute(query.format(**date_range))
             numrows = int(cursor.rowcount)
             
             for i in range(numrows):        
                 data = cursor.fetchone()
                 data['abtest'] = abtest
-                self.api.tag_abtest(data)
-            
+                
+                user_id = data['user_id']
+                del data['user_id']
+                                
+                created_at = data['created_at']
+                del data['created_at']
+                
+                if created_at > last_update:
+                    last_update = created_at
+                                                    
+                self.actions_api.tag_abtest(user_id, data)
+
+            self.import_api.set_last_update(query_name, last_update)            
             cursor.close() 
              
     def close(self):
@@ -113,71 +196,3 @@ class MysqlImporter(object):
         """
         
         self.mysql_conn.close()
-
-def get_mysql_conn():
-    """Helper to get mysql connection
-    
-    Depends on various os environment variables to 
-    be set.
-    
-    Returns:
-        MySQLdb connection
-        
-    """
-    
-    return MySQLdb.connect(
-            user=os.environ.get('MYSQL_USER'),
-            passwd= os.environ.get('MYSQL_PASS'),
-            db= os.environ.get('MYSQL_DB'),
-            host= os.environ.get('MYSQL_HOST', 'localhost'),
-            port= os.environ.get('MYSQL_PORT', 3307)) 
-    
-class MysqlImportHelper(object):
-    """Boilerplate wrapper for MysqlImporter
-     
-    Provides boiler plate db initialization via 
-    environment variables.
-     
-    Just need to provide the query with the helper. Not too
-    flexible, but I don't need much more for importing.            
-    """
-    
-    def __init__(self):
-
-        mongo_db = get_mongodb_db()            
-        conn = get_mysql_conn()        
-        
-        api = ActionsApi(mongo_db)
-        self.importer = MysqlImporter(conn, api)
-        
-    def import_users(self, query):
-        """Wrapper for MysqlImporter.import_users
-        
-        Args:
-            query: Query used to import users
-        """
-            
-        self.importer.import_users(query)
-            
-    def import_actions(self, name, query):
-        """Wrapper for MysqlImporter.import_actions
-    
-        Args:
-            name: Action name
-            query: Query used to import actions
-        """
-        
-        self.importer.import_actions(name, query) 
-   
-    def import_abtests(self, abtest, query):
-        """Wrapper for MysqlImporter.import_actions
-    
-        Args:
-            abtest: Abtest name
-            query: Query used to import actions
-        """
-        
-        self.importer.import_abtests(abtest, query) 
-                   
-    def close(self):
-        self.importer.close()
