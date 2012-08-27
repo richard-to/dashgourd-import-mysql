c5a3e8b342bb562af867fe72f3454c569beda789
Richard To <gohn76@gmail.com>
2012-08-26 21:54:03 -0800

    Remove comments. Add option to pass in importer class.

--- dashgourd/importer/helper.py
+++ dashgourd/importer/helper.py
@@ -3,12 +3,8 @@ import os
 import subprocess
 from dashgourd.importer.mysql import MysqlImporter
 from pytz import timezone
+
 def get_importer_from_env():
-    """Shortcut to initialize MysqlImporter from env vars
-    
-    Returns:
-        MysqlImporter
-    """
 
     return MysqlImporter(
         os.environ.get('MYSQL_URI'), 
@@ -17,75 +13,61 @@ def get_importer_from_env():
         timezone(os.environ.get('MYSQL_TZ')))
         
     
-def import_users(query_name, query):
-    """Boilerplate for loading user import scripts
+def import_users(query_name, query, importer=None):
     
-    This uses os environment variables for the mysql and mongodb 
-    uris.
-    
-    Main reason for this is to avoid copying and pasting this boilerplate 
-    code in dozens of query files. Granted you could just have them all 
-    in a single file. That may actually be simpler.
-    
-    Args:
-        query_name: Name of query. Needed for import api to track last_update
-        query: Query to execute with format tokens {start} and {end}
-    """
-    
-    importer = get_importer_from_env()
+    auto_close = False
+    if importer is None:
+        importer = get_importer_from_env()
+        auto_close = True
     importer.import_users(query_name, query)
-    importer.close()
     
-def import_actions(action_name, query, query_name=None, unique=False):
-    """Boilerplate for loading actions from import scripts
+    if auto_close:
+        importer.close()
+
+def import_profile(query_name, query, importer=None):
     
-    See import_users method for more info.
-    """
+    auto_close = False
+    if importer is None:
+        importer = get_importer_from_env()
+        auto_close = True
+    importer.import_profile(query_name, query)
+    
+    if auto_close:
+        importer.close()
+    
+
+def import_actions(action_name, query, query_name=None, 
+        unique=False, importer=None):
     
     if query_name is None:
         query_name = "{}_{}".format('action', action_name)
         
-    importer = get_importer_from_env()
+    auto_close = False
+    if importer is None:
+        importer = get_importer_from_env()
+        auto_close = True
+
     importer.import_actions(action_name, query_name, query, unique)
-    importer.close()
+   
+    if auto_close:
+        importer.close()
     
-def import_abtests(abtest, query, query_name=None):
-    """Boilerplate for loading abtests from import scripts
-    
-    See import_users method for more info. 
-    """
+def import_abtests(abtest, query, query_name=None, importer=None):
 
     if query_name is None:
         query_name = "{}_{}".format('ab', abtest)
             
-    importer = get_importer_from_env()
+    auto_close = False
+    if importer is None:
+        importer = get_importer_from_env()
+        auto_close = True
+
     importer.import_abtests(abtest, query_name, query)
-    importer.close()
+
+    if auto_close:
+        importer.close()
     
 def run_scripts(location, args=[], whitelist=None):
-    """Runs import scripts at specified folder location.
-    
-    This is a helper classs in the case that you split import 
-    scripts into multiple files. And don't want to manually run 
-    each script.
-    
-    This is helpful if you are evaluating how to import data and 
-    what format the data should be in.
-    
-    Additionally this could be used to update the system in batch 
-    instead of sending events as they come.
-    
-    This assumes the scripts are self contained and will send the 
-    appropriate data to DashGourd.
-    
-    This function is not completely tied to import scripts. Can also 
-    be used to execute any kind of file.
-    
-    Args:
-        location: Can be a single file or a directory. Not recursive.
-        args: An optional list of arguments to pass into the script
-        whitelist: A list of files to run in the directory.
-    """
 
     cmd_args = [0]
     cmd_args.extend(args)
